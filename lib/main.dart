507d62e6d613f119715345a70fa2491961735e82
Nirad <yeola.nirad@gmail.com>
2021-05-15 01:56:17 +0530

    Made simple changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'home.dart';
 
 void main() {
@@ -10,8 +9,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Home(),
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF1D1E33),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
         accentColor: Colors.purple,
